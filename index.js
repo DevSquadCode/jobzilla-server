c0916e094751ebd65ac9d7bb9dcaec079aed3535
shadow-chaser <ashabhtanim@gmail.com>
2021-07-28 15:49:59 +0600

    get testimonials

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fileUpload = require('express-fileupload');
+// const fileUpload = require('express-fileupload');
 const ObjectId = require("mongodb").ObjectID;
 require('dotenv').config()
 const port = 8080
@@ -9,13 +9,35 @@ const port = 8080
 const app = express()
 app.use(cors());
 app.use(bodyParser.json());
-app.use(fileUpload());
-        
+// app.use(fileUpload());
+
+
+
+const { MongoClient } = require('mongodb');
+const uri = `mongodb+srv://devSquad:${process.env.DB_PASS}@cluster0.133bl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const testimonialCollection = client.db(`${process.env.DB_NAME}`).collection("testimonials");
+    // perform actions on the collection object3
+
+    app.get('/testimonials', (req, res) => {
+        testimonialCollection.find({})
+            .toArray((err, documents) => {
+                // console.log(documents);
+                res.send(documents);
+            })
+    });
+
+
+});
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-        
+
+
 app.listen(port, () => {
-    console.log('Example app listening')
-}) 
-        
+    console.log(`Example app listening at http://localhost:${port}`)
+})
